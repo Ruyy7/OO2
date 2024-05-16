e84208591acdf12cb4add9e2f7f2e9443bd5fb22
Ruyy7 <ruyyackerman@gmail.com>
2024-05-16 15:16:26 -0300

    Refactoring

---
+++ Refactoring/Ejercicio1/1.1/Protocolo de cliente.java
@@ -0,0 +1,31 @@
+/** 
+* Retorna el límite de crédito del cliente
+*/
+protected double lmtCrdt() {}
+
+    /** 
+    * Retorna el monto facturado al cliente desde la fecha f1 a la fecha f2
+    */
+    protected double mtFcE(LocalDate f1, LocalDate f2) {}
+    
+    /** 
+    * Retorna el monto cobrado al cliente desde la fecha f1 a la fecha f2
+    */
+    protected double mtCbE(LocalDate f1, LocalDate f2) {}
+
+// Nombre de los metodos poco descriptivos
+
+/** 
+* Retorna el límite de crédito del cliente
+*/
+protected double limiteCredito() {}
+
+    /** 
+    * Retorna el monto facturado al cliente desde la fecha f1 a la fecha f2
+    */
+    protected double montoFacturadoEntreFechas(LocalDate fechaInicio, LocalDate fechaFin) {}
+    
+    /** 
+    * Retorna el monto cobrado al cliente desde la fecha f1 a la fecha f2
+    */
+    protected double montoCobradoEntreFechas(LocalDate fechaInicio, LocalDate fechaFin) {}
