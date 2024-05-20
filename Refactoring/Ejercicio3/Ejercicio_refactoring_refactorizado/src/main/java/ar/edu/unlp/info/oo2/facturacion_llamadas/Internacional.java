befe7b333663d59166ff96af8b2e80d92a69976b
Ruyy <87097965+Ruyy7@users.noreply.github.com>
2024-05-20 18:28:49 -0300

    Update Internacional.java

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Internacional.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Internacional.java
@@ -5,13 +5,10 @@ public class Internacional extends Llamada {
     public Internacional(Cliente origen, Cliente destino, int duracion) {
         super(origen, destino, duracion);
     }
-<<<<<<< HEAD
-=======
 
     public double calcularMontoLlamada() {
         return this.getDuracion() * 150 + (this.getDuracion() * 150 * 0.21) + 50;
     }
->>>>>>> 393a5041df2a9c2ffd4f9aa952b0d77d1235ecdf
 
     public double calcularMontoLlamada() {
         return this.getDuracion() * 150 + (this.getDuracion() * 150 * 0.21) + 50;
