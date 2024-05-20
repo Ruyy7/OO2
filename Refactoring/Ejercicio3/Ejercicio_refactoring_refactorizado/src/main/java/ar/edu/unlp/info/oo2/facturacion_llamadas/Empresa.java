c6d302ea942eca6dcdc70d6ffd29be1669c92924
aguscabello <101902214+aguscabello@users.noreply.github.com>
2024-05-20 17:51:19 -0300

    Sacamos variables estáticas

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Empresa.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Empresa.java
@@ -8,8 +8,6 @@ public class Empresa {
 	private List<Llamada> llamadas = new ArrayList<Llamada>();
 	private GestorNumerosDisponibles guia = new GestorNumerosDisponibles();
 
-	static double descuentoJur = 0.15;
-	static double descuentoFis = 0;
 
 	public boolean agregarNumeroTelefono(String str) {
 		boolean encontre = guia.getLineas().contains(str);
