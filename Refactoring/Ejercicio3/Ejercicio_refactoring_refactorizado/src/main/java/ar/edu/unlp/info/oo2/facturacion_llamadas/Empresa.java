c5d4b1f51440e5d72523c2bc04e0861024092b41
Ruyy <87097965+Ruyy7@users.noreply.github.com>
2024-05-20 18:28:02 -0300

    Update Empresa.java

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Empresa.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Empresa.java
@@ -7,11 +7,7 @@ public class Empresa {
 	private List<Cliente> clientes = new ArrayList<Cliente>();
 	private List<Llamada> llamadas = new ArrayList<Llamada>();
 	private GestorNumerosDisponibles guia = new GestorNumerosDisponibles();
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 393a5041df2a9c2ffd4f9aa952b0d77d1235ecdf
+	
 	public boolean agregarNumeroTelefono(String str) {
 		boolean encontre = guia.getLineas().contains(str);
 		if (!encontre) {
@@ -58,19 +54,10 @@ public class Empresa {
 
 	public double calcularMontoTotalLlamadas(Cliente cliente) {
 		return this.llamadas.stream()
-<<<<<<< HEAD
 	            .filter(llamada -> llamada.getOrigen().esIgual(cliente))
 	            .mapToDouble(llamada -> llamada.calcularMontoLlamada())
 	            .sum();
 	    }
-=======
-			.filter(llamada -> llamada.getOrigen().esIgual(cliente))
-			.mapToDouble(llamada -> llamada.calcularMontoLlamada())
-			.sum();
-			
-	}
-	
->>>>>>> 393a5041df2a9c2ffd4f9aa952b0d77d1235ecdf
 
 	public int cantidadDeUsuarios() {
 		return clientes.size();
