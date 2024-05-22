9d6dc07bca8f7a6f5a8cf670460f0f77d0f62913
Ruyy <87097965+Ruyy7@users.noreply.github.com>
2024-05-21 21:07:02 -0300

    Agrego constructor

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/PersonaJuridica.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/PersonaJuridica.java
@@ -1,9 +1,20 @@
 package ar.edu.unlp.info.oo2.facturacion_llamadas;
 
 public class PersonaJuridica extends Cliente {
+	private String cuit;
+	
+	public PersonaJuridica(String nombre, String numeroTelefono, String cuit) {
+		super(nombre, numeroTelefono);
+		this.cuit = cuit;
+	}
 
 	@Override
 	public double aplicarDescuento() {
 		return 0.15;
 	}
+	
+	public String getCuit() {
+		return cuit;
+	}
+	
 }
