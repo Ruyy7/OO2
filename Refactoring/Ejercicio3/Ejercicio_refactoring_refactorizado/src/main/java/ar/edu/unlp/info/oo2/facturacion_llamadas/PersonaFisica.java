a39fa95b165a1e4ddb515ac89556e6954bc365d8
Ruyy <87097965+Ruyy7@users.noreply.github.com>
2024-05-21 21:06:24 -0300

    Agrego constructor

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/PersonaFisica.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/PersonaFisica.java
@@ -1,10 +1,20 @@
 package ar.edu.unlp.info.oo2.facturacion_llamadas;
 
 public class PersonaFisica extends Cliente {
+	private String dni;
+	
+	public PersonaFisica(String nombre, String numeroTelefono, String dni) {
+		super(nombre, numeroTelefono);
+		this.dni = dni;
+	}
 
 	@Override
 	public double aplicarDescuento() {
 		return 0;
 	}
 
+	public String getDNI() {
+		return dni;
+	}
+	
 }
