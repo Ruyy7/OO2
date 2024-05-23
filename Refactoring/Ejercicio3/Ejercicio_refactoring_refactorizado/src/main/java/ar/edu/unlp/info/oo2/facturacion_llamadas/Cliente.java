7ee2930a0db96c96a79d99c7c9110750c07fd66a
Ruyy <87097965+Ruyy7@users.noreply.github.com>
2024-05-23 14:51:45 -0300

    Muevo el registrar llamadas a la clase cliente

--- Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Cliente.java
+++ Refactoring/Ejercicio3/Ejercicio_refactoring_refactorizado/src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/Cliente.java
@@ -6,15 +6,29 @@ import java.util.List;
 public abstract class Cliente {
 	public List<Llamada> llamadas = new ArrayList<Llamada>();
 	private String nombre;
+	private String numeroTelefono;
 	
 	public Cliente(String nombre, String numeroTelefono) {
 		this.nombre = nombre;
 		this.numeroTelefono = numeroTelefono;
 	}
 
-	private String numeroTelefono;
-	
 	public abstract double aplicarDescuento();
+
+    	@Deprecated()
+	public Llamada registrarLlamada(Cliente origen, Cliente destino, String t, int duracion) {
+		Llamada llamada = new Llamada(t, origen.getNumeroTelefono(), destino.getNumeroTelefono(), duracion);
+		llamadas.add(llamada);
+		origen.llamadas.add(llamada);
+		return llamada;
+	}
+	
+	public Llamada registrarLlamada(CreadorLlamada creadorLlamada, Cliente origen, Cliente destino, int duracion) {
+		Llamada llamada = creadorLlamada.crearLlamada(origen,destino,duracion);
+		llamadas.add(llamada);
+		origen.llamadas.add(llamada);
+		return llamada;
+	}
 	
 	public boolean esIgual(Cliente otroCliente) {
 		return this.numeroTelefono.equals(otroCliente.getNumeroTelefono());
