0fb3abf0ff7f6af702834cf9e277d23d3181d0da
Ruyy7 <ruyyackerman@gmail.com>
2024-05-28 15:11:10 -0300

    Composcion

---
+++ Frameworks/Ejercicio2/Composicion/ic-composicion/src/main/java/ar/edu/unlp/info/oo2/rw/example/SimpleGame.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo2.rw.example;
+
+import ar.edu.unlp.info.oo2.rw.model.*;
+
+public class SimpleGame {
+    public static void main(String[] args) {
+        GameBoard board = new GameBoard();
+        board.add(new Robot("Twonky", new Caterpillar(), new NuclearPlant(), new BombsSystem()));
+        board.add(new Robot("Hammer Bot", new Overcraft(), new NuclearPlant(), new LasersSystem()));
+        board.runForCicles(5);
+    }
+}
